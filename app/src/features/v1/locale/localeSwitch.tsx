823c2f1ddde0aa87e411d39e6b5ddcfdc9e6c4f1
wujun4code <wujun19890209@gmail.com>
2023-03-20 22:38:25 +0000

    feat: add api layer.

--- app/src/features/v1/locale/localeSwitch.tsx
+++ app/src/features/v1/locale/localeSwitch.tsx
@@ -1,10 +1,24 @@
-import { Switch, HStack } from '@chakra-ui/react'
+import { Switch, Box, Spacer, HStack, SimpleGrid, FormControl, Grid, Flex, FormLabel } from '@chakra-ui/react'
+
+export interface TextSwicthProps {
+    text: string;
+    id: string;
+}
+
+export function TextSwicth(props: TextSwicthProps) {
+    return (
+        <Flex>
+            <FormLabel htmlFor={props.id}>{props.text}</FormLabel>
+            <Switch id={props.id} size='lg' defaultChecked />
+        </Flex>
+    )
+}
 
 export function LanguageSwitchs() {
+    const languages = [{ id: 'lan-1', text: 'English' }, { id: 'lan-2', text: '中文简体' }];
     return (
-        <HStack>
-            <Switch size='md'/>
-            <Switch size='md' />
-        </HStack>
+        <FormControl padding='0px 10px' as={HStack}>
+            {languages.map(data => (<TextSwicth text={data.text} id={data.id}></TextSwicth>))}
+        </FormControl>
     )
 }
--- app/src/features/v1/top-nav/promptScopeTopNav.tsx
+++ app/src/features/v1/top-nav/promptScopeTopNav.tsx
@@ -2,7 +2,7 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 export function PromptScopeTopNavTabs() {
     return (
-        <Tabs variant='soft-rounded' colorScheme='green'>
+        <Tabs variant='soft-rounded' colorScheme='blue'>
         <TabList>
           <Tab>Tab 1</Tab>
           <Tab>Tab 2</Tab>
