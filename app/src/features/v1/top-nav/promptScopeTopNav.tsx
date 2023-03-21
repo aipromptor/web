275edee129674fbf888e596082eb7c094d3230b1
Wu Jun <wujun4code@gmail.com>
2023-03-21 21:53:41 +0800

    feat: add dynamic checked button.

--- app/src/features/v1/top-nav/promptScopeTopNav.tsx
+++ app/src/features/v1/top-nav/promptScopeTopNav.tsx
@@ -1,4 +1,5 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Button, HStack } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Button, HStack, } from '@chakra-ui/react'
+import { CheckIcon, AddIcon } from '@chakra-ui/icons'
 import { useAppSelector, useAppDispatch } from '../../../app/hooks';
 import { useEffect } from 'react';
 
@@ -7,7 +8,8 @@ import {
   scopeNavs,
   selectedIndex,
   fetchScopesAsync,
-  fetchPromptsBySelectTab
+  fetchPromptsBySelectTab,
+  togglePrompt,
 } from './promptScopeSlice';
 
 
@@ -27,10 +29,16 @@ export function PromptScopeTopNavTabs() {
         {tabs.map(t => (<Tab key={t.id} id={t.id}>{t.text}</Tab>))}
       </TabList>
       <TabPanels>
-        {tabs.map((t, index) => (
-          <TabPanel key={index}>
+        {tabs.map((t, ti) => (
+          <TabPanel key={ti}>
             <HStack>
-              {t.prompts.map(p => (<Button variant='outline'>{p.title}</Button>))}
+              {t.prompts.map((p, pi) => (
+                <Button key={p.id} onClick={(e) => dispatch(togglePrompt({ scopeIndex: ti, promptIndex: pi }))}
+                  backgroundColor={p.selected ? 'green.300' : ''}
+                  color={p.selected ? 'white' : ''}
+                  leftIcon={p.selected ? <CheckIcon /> : <AddIcon />}
+                  variant='outline'>{p.title}</Button>
+              ))}
             </HStack>
 
           </TabPanel>))}
